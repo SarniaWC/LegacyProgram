2224c85cf41a11aa79bf58e2d426740cd41ec7d8
hjwitt <hjwittliff@gmail.com>
2025-09-06 21:52:11 -0400

    replaced `HomeLayout` with `StandardLayout` for consistent structure across layouts

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { main_site } from "@/constants";
 import Link from "next/link";
+import StandardLayout from "@/components/StandardLayout";
 
 config.autoAddCss = false;
 
@@ -12,58 +13,5 @@ interface LayoutProps {
 }
 
 export default function HomeLayout({ children }: LayoutProps) {
-  return (
-    <>
-      <header className="p-6">
-        <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center">
-          <div className={"flex items-center"}>
-            <a
-              href={main_site}
-              className="flex items-center hover:text-primary"
-            >
-              Sarnia Wrestling Club
-            </a>
-          </div>
-          <div className={"flex flex-col sm:flex-row"}>
-            <ul className="flex gap-4">
-              <li>
-                <a
-                  href="https://www.facebook.com/groups/95004813674"
-                  className={"hover:text-primary"}
-                >
-                  <FontAwesomeIcon icon={faFacebook} />
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://www.instagram.com/sarniabluewaterwc/"
-                  className={"hover:text-primary"}
-                >
-                  <FontAwesomeIcon icon={faInstagram} />
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <nav>
-          <div className="max-w-screen-xl flex items-center justify-center mx-auto py-4 px-4 sm:px-6 lg:px-8">
-            <div className="flex flex-col sm:flex-row">
-              <ul className="flex gap-4">
-                <li className="hover:text-primary">
-                  <Link href="/">Home</Link>
-                </li>
-                <li className="hover:text-primary">
-                  <a href={`${main_site}/about`}>About</a>
-                </li>
-                <li className="hover:text-primary">
-                  <a href={`${main_site}/contact`}>Contact</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-      </header>
-      <main>{children}</main>
-    </>
-  );
+  return <StandardLayout>{children}</StandardLayout>;
 }
--- src/app/p/[id]/detail/layout.tsx
+++ src/app/p/[id]/detail/layout.tsx
@@ -1,17 +1,16 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { main_site } from "@/constants";
-import HomeLayout from "@/app/(home)/layout";
+import StandardLayout from "@/components/StandardLayout";
 
 interface LayoutProps {
-    children?: React.ReactNode;
+  children?: React.ReactNode;
 }
 
 export default function Layout({ children }: LayoutProps) {
-    return (
-        <>
-            <HomeLayout>
-            {children}
-            </HomeLayout>
-        </>
-    )}
+  return (
+    <>
+      <StandardLayout>{children}</StandardLayout>
+    </>
+  );
+}
